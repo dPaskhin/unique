fc21dff0140e95dd6138f8b85000f247623dcdf6
Dmitrii Paskhin <d.pasxin@gmail.com>
2024-11-21 23:29:10 +0100

    feat: handle non-functional value

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,25 +1,33 @@
 /**
  * Configuration options for the unique value generation process.
  *
- * @template Fn The type of the function passed to generate values.
- *
- * @property {Set<unknown>} [store] - Optional store to keep track of unique values. If a stringifier is provided, the store is a `Set<string>`. Otherwise, it's a `Set<ReturnType<Fn>>`.
- * @property {number} [maxRetries=50] - The maximum number of retries allowed to generate a unique value before throwing an error. Defaults to 50.
- * @property {number} [maxTime=50] - The maximum time allowed (in milliseconds) for generating a unique value before throwing an error. Defaults to 50ms.
- * @property {ReturnType<Fn>[]} [exclude] - A list of values to be excluded from the result set. When a stringifier is provided, the list will be stringified.
- * @property {(value: ReturnType<Fn>) => string} [stringifier] - A function to stringify a result value before storing it in the set and checking for uniqueness. Defaults to {@link JSON.stringify}.
+ * @template Value The type of the value being processed for uniqueness.
  */
-type IOptions<Fn extends (...args: any) => any> = {
+type IOptions<Value> = {
+  /**
+   * Optional store to keep track of unique values.
+   */
   store?: Set<unknown>;
+  /**
+   * The maximum number of retries allowed to generate a unique value before throwing an error. Defaults to 50.
+   */
   maxRetries?: number;
+  /**
+   * The maximum time allowed (in milliseconds) for generating a unique value before throwing an error. Defaults to 50ms.
+   */
   maxTime?: number;
-  exclude?: ReturnType<Fn>[];
-  stringifier?: (value: ReturnType<Fn>) => string;
+  /**
+   * A list of values to be excluded from the result set. When a stringifier is provided, the list will be stringified.
+   */
+  exclude?: Value[];
+  /**
+   * A function to stringify a result value before storing it in the set and checking for uniqueness. Defaults to {@link JSON.stringify}.
+   */
+  stringifier?: (value: Value) => string;
 };
 
 /**
  * Creates a function that generates unique values based on the passed function `fn`.
- * Ensures uniqueness by storing generated values and retrying if duplicates are found.
  *
  * @template Fn The type of the function passed to generate values.
  *
@@ -47,7 +55,7 @@ type IOptions<Fn extends (...args: any) => any> = {
  */
 export function uniqueFactory<Fn extends (...args: any[]) => any>(
   fn: Fn,
-  options: IOptions<Fn> = {}
+  options: IOptions<ReturnType<Fn>> = {}
 ): (...args: Parameters<Fn>) => ReturnType<Fn> {
   let {
     store = new Set(),
@@ -65,28 +73,23 @@ export function uniqueFactory<Fn extends (...args: any[]) => any>(
     let startTime = Date.now();
 
     while (true) {
-      let now = Date.now();
-      let duration = now - startTime;
+      let duration = Date.now() - startTime;
 
       if (duration >= maxTime) {
-        throw new Error(
-          createErrorMessage(
-            'Exceeded maxTime: ' + maxTime,
-            store.size,
-            duration,
-            currentIterations
-          )
+        throw new UniqueError(
+          `Exceeded maxTime (${maxTime})`,
+          store.size,
+          duration,
+          currentIterations
         );
       }
 
       if (currentIterations >= maxRetries) {
-        throw new Error(
-          createErrorMessage(
-            'Exceeded maxTries: ' + maxRetries,
-            store.size,
-            duration,
-            currentIterations
-          )
+        throw new UniqueError(
+          `Exceeded maxTries (${maxRetries})`,
+          store.size,
+          duration,
+          currentIterations
         );
       }
 
@@ -110,8 +113,7 @@ export function uniqueFactory<Fn extends (...args: any[]) => any>(
 }
 
 /**
- * A global store to track unique values across multiple invocations of the `unique` function.
- * This can be shared across different parts of the code to ensure unique values globally.
+ * A global store to track unique values across the application.
  *
  * @example
  * ```ts
@@ -133,19 +135,17 @@ export const GLOBAL_STORE = new Set();
 
 /**
  * Generates a unique value using the provided function `fn` that takes no arguments.
- * Ensures uniqueness by checking against previous results stored either in the provided `store` or the global store.
  *
- * **Note:** The global store is shared across multiple invocations, and values will persist globally.
- * If you need isolation between different parts of your application, you should provide a custom `store` in the options.
+ * **Note:** The global store is used by default and shared across the application.
+ * For isolated usage, provide a custom `store` in the options.
  *
- * **Best Practice:** Avoid using the global store in environments where global uniqueness is not needed,
- * as it may lead to unexpected results due to shared state.
+ * **Best Practice:** Use a custom store to avoid shared state and unexpected results.
  *
  * @template Fn The type of the function that generates values without arguments.
  *
  * @param {Fn} fn - The function to generate values. This function does not accept any arguments.
  * @param {never[]} [args] - No arguments are passed to functions that do not accept arguments.
- * @param {IOptions<Fn>} [options] - Optional configuration for controlling the uniqueness generation process.
+ * @param {IOptions<ReturnType<Fn>>} [options] - Optional configuration for controlling the uniqueness generation process.
  * @returns {ReturnType<Fn>} The unique value generated by the function `fn`.
  *
  * @throws {Error} Throws an error if the max retries or max time is exceeded.
@@ -155,31 +155,29 @@ export const GLOBAL_STORE = new Set();
  * import { unique } from '@dpaskhin/unique';
  * import { faker } from '@faker-js/faker';
  *
- * // Outputs a unique user's email
- * console.log(unique(faker.internet.email));
+ * // Outputs a unique city name
+ * console.log(unique(faker.location.city));
  * ```
  */
 export function unique<Fn extends () => any>(
   fn: Fn,
   args?: never[],
-  options?: IOptions<Fn>
+  options?: IOptions<ReturnType<Fn>>
 ): ReturnType<Fn>;
 
 /**
  * Generates a unique value using the provided function `fn` that accepts arguments.
- * Ensures uniqueness by checking against previous results stored either in the provided `store` or the global store.
  *
- * **Note:** The global store is shared across multiple invocations, and values will persist globally.
- * If you need isolation between different parts of your application, you should provide a custom `store` in the options.
+ * **Note:** The global store is used by default and shared across the application.
+ * For isolated usage, provide a custom `store` in the options.
  *
- * **Best Practice:** Avoid using the global store in environments where global uniqueness is not needed,
- * as it may lead to unexpected results due to shared state.
+ * **Best Practice:** Use a custom store to avoid shared state and unexpected results.
  *
  * @template Fn The type of the function that generates values and accepts arguments.
  *
  * @param {Fn} fn - The function to generate values. This function must accept arguments.
  * @param {Parameters<Fn>} args - The arguments to be passed to the function `fn`.
- * @param {IOptions<Fn>} [options] - Optional configuration for controlling the uniqueness generation process.
+ * @param {IOptions<ReturnType<Fn>>} [options] - Optional configuration for controlling the uniqueness generation process.
  * @returns {ReturnType<Fn>} The unique value generated by the function `fn`.
  *
  * @throws {Error} Throws an error if the max retries or max time is exceeded.
@@ -203,33 +201,76 @@ export function unique<Fn extends () => any>(
 export function unique<Fn extends (...args: any[]) => any>(
   fn: Fn,
   args: Parameters<Fn>,
-  options?: IOptions<Fn>
+  options?: IOptions<ReturnType<Fn>>
 ): ReturnType<Fn>;
 
-export function unique<Fn extends (...args: any[]) => any>(
-  fn: Fn,
-  args: Parameters<Fn> = [] as unknown as Parameters<Fn>,
-  options: IOptions<Fn> = {}
-): ReturnType<Fn> {
+/**
+ * Ensures a unique value from the provided `value`, which is not a function.
+ * There's only one attempt to check the uniqueness of the `value`.
+ *
+ * **Note:** The global store is used by default and shared across the application.
+ * For isolated usage, provide a custom `store` in the options.
+ *
+ * **Best Practice:** Use a custom store to avoid shared state and unexpected results.
+ *
+ * @template Value The type of the value to ensure uniqueness.
+ *
+ * @param {NotFunction<Value>} value - The plain value to ensure uniqueness for. Must not be a function.
+ * @param {IOptions<Value>} [options] - Optional configuration for controlling the uniqueness generation process.
+ * @returns {Value} The unique value ensured by the `unique` function.
+ *
+ * @throws {Error} Throws an error if the value already exists in the store.
+ *
+ * @example
+ * ```ts
+ * import { unique } from '@dpaskhin/unique';
+ *
+ * // Each time outputs a unique ID string or throws an error
+ * console.log(unique(window.crypto.randomUUID()));
+ * console.log(unique(window.crypto.randomUUID()));
+ * console.log(unique(window.crypto.randomUUID()));
+ * ```
+ */
+export function unique<Value>(
+  value: NotFunction<Value>,
+  options?: Omit<IOptions<Value>, 'maxRetries'>
+): Value;
+
+export function unique<FnOrValue = any>(
+  fnOrValue: FnOrValue,
+  argsOrOptions?: FnOrValue extends () => any
+    ? Parameters<FnOrValue>
+    : IOptions<FnOrValue>,
+  options: IOptions<FnOrValue> = {} as IOptions<FnOrValue>
+): FnOrValue {
+  if (typeof fnOrValue === 'function') {
+    return uniqueFactory(
+      fnOrValue as () => FnOrValue,
+      Object.assign({}, options, { store: options.store || GLOBAL_STORE })
+    ).apply(null, (argsOrOptions as []) || []);
+  }
+
   return uniqueFactory(
-    fn,
-    Object.assign(options, { store: options.store || GLOBAL_STORE })
-  ).apply(null, args);
+    () => fnOrValue,
+    Object.assign({}, ((argsOrOptions as IOptions<FnOrValue>) ||= {}), {
+      store: (argsOrOptions as IOptions<FnOrValue>).store || GLOBAL_STORE,
+      maxRetries: 1,
+    })
+  )();
 }
 
-function createErrorMessage(
-  code: string,
-  storeSize: number,
-  duration: number,
-  currentIterations: number
-): string {
-  return `
-  ${code} for uniqueness check.
-    
-Found ${storeSize} unique entries before throwing error.
-retried: ${currentIterations}
-total time: ${duration}ms
-
-May not be able to generate any more unique values with current settings.
-Try adjusting maxTime or maxRetries parameters.`;
+class UniqueError extends Error {
+  constructor(
+    code: string,
+    size: number,
+    duration: number,
+    iterations: number
+  ) {
+    super(
+      `${code} - store size: ${size}, retried: ${iterations}, duration: ${duration}ms`
+    );
+    this.name = 'UniqueError';
+  }
 }
+
+type NotFunction<T> = T extends (...args: any[]) => any ? never : T;
---
+++ src/test/types/index.test-d.ts
@@ -0,0 +1,122 @@
+import { expectType } from 'tsd';
+import { GLOBAL_STORE, unique, uniqueFactory } from '../../main';
+
+const value: string = '';
+
+// @ts-expect-error
+function withArgs(foo: string, bar: number): number {
+  return 1;
+}
+
+function withoutArgs(): string {
+  return 'foo';
+}
+
+// Plain value
+// ----------------------------------------
+
+expectType<'foo'>(unique('foo'));
+
+expectType<1>(unique(1));
+
+// Infers valid return type.
+// @ts-expect-error
+expectType<number>(unique('foo'));
+
+// Must be provided at least one param.
+// @ts-expect-error
+unique();
+
+// Does not accept an array as second param (empty).
+// @ts-expect-error
+unique('foo', []);
+
+// Does not accept an array as second param (with value).
+// @ts-expect-error
+unique('foo', ['1']);
+
+// Does not accept maxRetries option.
+// @ts-expect-error
+unique('foo', { maxRetries: 51 });
+
+expectType<number>(unique(withArgs('foo', 1)));
+
+// Infers valid return type.
+// @ts-expect-error
+expectType<string>(unique(withArgs('foo', 1)));
+
+expectType<string>(unique(withoutArgs()));
+
+// Infers valid return type.
+// @ts-expect-error
+expectType<number>(unique(withoutArgs()));
+
+expectType<string>(unique(value));
+
+expectType<'foo'>(unique('foo', { store: GLOBAL_STORE }));
+
+expectType<1>(unique(1, { store: GLOBAL_STORE }));
+
+expectType<string>(unique(value, { store: GLOBAL_STORE }));
+
+// Exclude has to be the same as the return type.
+// @ts-expect-error
+unique(() => 1, [], { exclude: ['1'] });
+
+// Functions
+// ----------------------------------------
+
+expectType<string>(unique(withoutArgs));
+
+// Infers valid return type.
+// @ts-expect-error
+expectType<number>(unique(withoutArgs));
+
+// Does not accept any params through the params array.
+// @ts-expect-error
+unique(withoutArgs, ['1']);
+
+// Does not accept options as second param.
+// @ts-expect-error
+unique(withoutArgs, { store: GLOBAL_STORE });
+
+expectType<string>(unique(withoutArgs, [], { store: GLOBAL_STORE }));
+
+expectType<number>(unique(withArgs, ['foo', 1]));
+
+// Infers valid return type.
+// @ts-expect-error
+expectType<string>(unique(withArgs, ['foo', 1]));
+
+expectType<number>(unique(withArgs, ['foo', 1], { store: GLOBAL_STORE }));
+
+// Params must be provided.
+// @ts-expect-error
+unique(withArgs);
+
+// Does not accept options as second param.
+// @ts-expect-error
+unique(withArgs, { store: GLOBAL_STORE });
+
+// Global store
+// ----------------------------------------
+expectType<Set<unknown>>(GLOBAL_STORE);
+
+// Factory
+// ----------------------------------------
+
+expectType<(foo: string, bar: number) => number>(uniqueFactory(withArgs));
+
+expectType<() => string>(uniqueFactory(withoutArgs));
+
+expectType<() => string>(uniqueFactory(withoutArgs, { store: GLOBAL_STORE }));
+
+expectType<() => void>(uniqueFactory(() => {}));
+
+// Exclude has to be the same as the return type.
+// @ts-expect-error
+uniqueFactory(() => {}, { exclude: ['1'] });
+
+// Must be provided at least one param.
+// @ts-expect-error
+uniqueFactory();
